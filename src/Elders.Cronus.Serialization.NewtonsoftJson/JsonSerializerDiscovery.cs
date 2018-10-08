5b527c4ff89ad50ed7e1ef2f5d2e738995ed4e3a
mynkow <mynkow@gmail.com>
2018-10-08 11:40:58 +0300

    Adds JsonSerializerDiscovery

---
+++ src/Elders.Cronus.Serialization.NewtonsoftJson/JsonSerializerDiscovery.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Elders.Cronus.Discoveries;
+using Elders.Cronus.Serialization.NewtonsoftJson;
+using Elders.Cronus.Serializer;
+
+namespace Elders.Cronus.Pipeline.Config
+{
+    public class JsonSerializerDiscovery : DiscoveryBasedOnExecutingDirAssemblies<ISerializer>
+    {
+        protected ISerializer GetSerializer(DiscoveryContext context)
+        {
+            List<Type> contracts = context.Assemblies
+                .Where(asm => ReferenceEquals(default(BoundedContextAttribute), asm.GetAssemblyAttribute<BoundedContextAttribute>()) == false)
+                .SelectMany(ass => ass.GetExportedTypes())
+                .ToList();
+
+            //contracts.AddRange(typeof(IMessage).Assembly.GetExportedTypes());
+            //contracts.AddRange(typeof(CronusAssembly).Assembly.GetExportedTypes());
+
+            return new JsonSerializer(contracts);
+        }
+
+        protected override DiscoveryResult<ISerializer> DiscoverFromAssemblies(DiscoveryContext context)
+        {
+            var result = new DiscoveryResult<ISerializer>();
+            result.Models.Add(new DiscoveredModel()
+            {
+                AbstractionType = typeof(ISerializer),
+                ImplementationType = typeof(JsonSerializer),
+                Instance = GetSerializer(context)
+            });
+
+            return result;
+        }
+    }
+}
+
